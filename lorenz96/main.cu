58ca9a9692e90f2384fa5ceb7d8b31b74aa98d94
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2018-07-30 16:56:35 +0100

    Made some improvements to Lorenz '96 code

--- lorenz96/main.cu
+++ lorenz96/main.cu
@@ -6,13 +6,13 @@
 __constant__ int N;
 
 // Timestep
-__constant__ float dt;
+__constant__ PREC dt;
 
 // Forcing
-__constant__ float F;
+__constant__ PREC F;
 
 // ODE device function
-__device__ float dXdT(float x__2, float x__1, float x, float x_1) {
+__device__ PREC dXdT(PREC x__2, PREC x__1, PREC x, PREC x_1) {
     return (x_1 - x__2)*x__1 - x + F;
 }
 
@@ -22,12 +22,12 @@ __device__ int shft(int n, int m) {
 }
 
 // Main step kernel function
-__global__ void step(float* __restrict__ in, float* __restrict__ out) {
+__global__ void step(PREC* __restrict__ in, PREC* __restrict__ out) {
     // Get global thread ID
     int tid = threadIdx.x + blockDim.x*blockIdx.x;
 
     // Intermediate steps
-    float k1, k2;
+    PREC k1, k2;
 
     if (tid < N) {
         // Compute k1
@@ -42,19 +42,25 @@ __global__ void step(float* __restrict__ in, float* __restrict__ out) {
         k2 = dXdT(in[shft(tid,-2)], in[shft(tid,-1)], in[tid], in[shft(tid,1)]);
 
         // Get local state
-        out[tid] = in[tid] + 0.5f*dt*(k2 - k1);
+        out[tid] = in[tid] + 0.5*dt*(k2 - k1);
     }
 }
 
 int main(int argc, const char **argv) {
-    int h_N = 64;
-    float h_dt = 0.01f;
-    float h_F = 8.0f;    
-    float *h_state, *h_hist, *d_prev, *d_next, *d_temp;
+    // Simulation parameters
+    int h_N = 40;
+    PREC h_dt = 0.05;
+    PREC h_F = 10.0;
     int length = 1000;
 
-    // Initialise card
+    // Storage vectors
+    PREC *h_state, *h_hist, *d_prev, *d_next, *d_temp;
 
+    // Kernel parameters
+    int nThreadsPerBlock = 64;
+    int nBlocks = 1 + ((h_N - 1)/nThreadsPerBlock);
+
+    // Initialise card
     findCudaDevice(argc, argv);
 
     // Move global constants to device
@@ -63,31 +69,34 @@ int main(int argc, const char **argv) {
     checkCudaErrors(cudaMemcpyToSymbol(F, &h_F, sizeof(h_F)));
 
     // Allocate memory on host and device
-    h_state = (float *)malloc(sizeof(float)*h_N);
-    h_hist  = (float *)malloc(sizeof(float)*length);
+    h_state = (PREC *)malloc(sizeof(PREC)*h_N);
+    h_hist  = (PREC *)malloc(sizeof(PREC)*length);
 
-    checkCudaErrors(cudaMalloc((void **)&d_prev, sizeof(float)*h_N));
-    checkCudaErrors(cudaMalloc((void **)&d_next, sizeof(float)*h_N));
+    checkCudaErrors(cudaMalloc((void **)&d_prev, sizeof(PREC)*h_N));
+    checkCudaErrors(cudaMalloc((void **)&d_next, sizeof(PREC)*h_N));
 
     // Set initial conditions
     for (int i = 0; i < h_N; i++) {
-        h_state[i] = rand()/RAND_MAX;
+        h_state[i] = (PREC)rand()/RAND_MAX;
     }
 
+    printf("%f %f\n", h_state[0], h_state[1]);
+
     // Copy initial conditions to device
-    checkCudaErrors(cudaMemcpy(d_prev, h_state, sizeof(float)*h_N, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_prev, h_state, sizeof(PREC)*h_N, cudaMemcpyHostToDevice));
 
     // Set initial condition in history array
     h_hist[0] = h_state[0];
 
     // Run forecast
+    printf("Running forecast with %d blocks and %d threads per block\n", nBlocks, nThreadsPerBlock);
     for (int i = 1; i < length; i++) {
         // Step forward once
-        step<<<ceil(h_N/64), 64>>>(d_prev, d_next);
+        step<<<nBlocks, nThreadsPerBlock>>>(d_prev, d_next);
         getLastCudaError("step execution failed\n");
 
         // Store one variable
-        checkCudaErrors(cudaMemcpy(&h_hist[i], &d_next[0], sizeof(float), cudaMemcpyDeviceToHost));
+        checkCudaErrors(cudaMemcpy(&h_hist[i], &d_next[0], sizeof(PREC), cudaMemcpyDeviceToHost));
 
         printf("%f\n", h_hist[i]);
 
@@ -96,7 +105,7 @@ int main(int argc, const char **argv) {
     }
 
     // Copy back results
-    checkCudaErrors(cudaMemcpy(h_state, d_next, sizeof(float)*h_N,cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(h_state, d_next, sizeof(PREC)*h_N,cudaMemcpyDeviceToHost));
 
     // Free up memory
     free(h_state);
